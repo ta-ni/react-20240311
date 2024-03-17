027385a0950f484459d0d4f64cff5019d2af7902
ta-ni <t.klymets@gmail.com>
2024-03-17 08:53:36 +0000

    add render restaurants data using React.createElement.

--- src/main.jsx
+++ src/main.jsx
@@ -96,3 +96,146 @@ root.render(<main>
         </ul>
     </div>
 </main>);
+
+// root.render(React.createElement('main', {
+//     children: [
+//         React.createElement('div', {
+//             children: [
+//                 React.createElement('h2', {
+//                     children: restaurants[0].name
+//                 }),
+//                 React.createElement('h3', {
+//                     children: 'Меню'
+//                 }),
+//                 React.createElement('ul', {
+//                     children: [
+//                         React.createElement('li', {
+//                             children: restaurants[0].menu[0].name
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[0].menu[1].name
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[0].menu[2].name
+//                         })
+//                     ]
+//                 }),
+//                 React.createElement('h3', {
+//                     children: 'Отзывы'
+//                 }),
+//                 React.createElement('ul', {
+//                     children: [
+//                         React.createElement('li', {
+//                             children: restaurants[0].reviews[0].text
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[0].reviews[1].text
+//                         })
+//                     ]
+//                 }),
+//             ]
+//         }),
+//         React.createElement('div', {
+//             children: [
+//                 React.createElement('h2', {
+//                     children: restaurants[1].name
+//                 }),
+//                 React.createElement('h3', {
+//                     children: 'Меню'
+//                 }),
+//                 React.createElement('ul', {
+//                     children: [
+//                         React.createElement('li', {
+//                             children: restaurants[1].menu[0].name
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[1].menu[1].name
+//                         })
+//                     ]
+//                 }),
+//                 React.createElement('h3', {
+//                     children: 'Отзывы'
+//                 }),
+//                 React.createElement('ul', {
+//                     children: [
+//                         React.createElement('li', {
+//                             children: restaurants[1].reviews[0].text
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[1].reviews[1].text
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[1].reviews[2].text
+//                         })
+//                     ]
+//                 }),
+//             ]
+//         }),
+//         React.createElement('div', {
+//             children: [
+//                 React.createElement('h2', {
+//                     children: restaurants[2].name
+//                 }),
+//                 React.createElement('h3', {
+//                     children: 'Меню'
+//                 }),
+//                 React.createElement('ul', {
+//                     children: [
+//                         React.createElement('li', {
+//                             children: restaurants[2].menu[0].name
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[2].menu[1].name
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[2].menu[2].name
+//                         })
+//                     ]
+//                 }),
+//                 React.createElement('h3', {
+//                     children: 'Отзывы'
+//                 }),
+//                 React.createElement('ul', {
+//                     children: [
+//                         React.createElement('li', {
+//                             children: restaurants[2].reviews[0].text
+//                         })
+//                     ]
+//                 }),
+//             ]
+//         }),
+//         React.createElement('div', {
+//             children: [
+//                 React.createElement('h2', {
+//                     children: restaurants[3].name
+//                 }),
+//                 React.createElement('h3', {
+//                     children: 'Меню'
+//                 }),
+//                 React.createElement('ul', {
+//                     children: [
+//                         React.createElement('li', {
+//                             children: restaurants[3].menu[0].name
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[3].menu[1].name
+//                         })
+//                     ]
+//                 }),
+//                 React.createElement('h3', {
+//                     children: 'Отзывы'
+//                 }),
+//                 React.createElement('ul', {
+//                     children: [
+//                         React.createElement('li', {
+//                             children: restaurants[3].reviews[0].text
+//                         }),
+//                         React.createElement('li', {
+//                             children: restaurants[3].reviews[1].text
+//                         })
+//                     ]
+//                 }),
+//             ]
+//         })
+//     ]
+// }));
