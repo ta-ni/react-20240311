0722528beff4a08b0901ef945ff37e9a84c1458b
ta-ni <t.klymets@gmail.com>
2024-03-17 08:52:50 +0000

    render restaurants data using jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,98 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { restaurants } from "./constants/mock.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render()
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<main>
+    <div>
+        <h2>{restaurants[0].name}</h2>
+        <h3>Меню</h3>
+        <ul>
+            <li>
+                {restaurants[0].menu[0].name}
+            </li>
+            <li>
+                {restaurants[0].menu[1].name}
+            </li>
+            <li>
+                {restaurants[0].menu[2].name}
+            </li>
+        </ul>
+        <h3>Отзывы</h3>
+        <ul>
+            <li>
+                {restaurants[0].reviews[0].text}
+            </li>
+            <li>
+                {restaurants[0].reviews[1].text}
+            </li>
+        </ul>
+    </div>
+    <div>
+        <h2>{restaurants[1].name}</h2>
+        <h3>Меню</h3>
+        <ul>
+            <li>
+                {restaurants[1].menu[0].name}
+            </li>
+            <li>
+                {restaurants[1].menu[1].name}
+            </li>
+        </ul>
+        <h3>Отзывы</h3>
+        <ul>
+            <li>
+                {restaurants[1].reviews[0].text}
+            </li>
+            <li>
+                {restaurants[1].reviews[1].text}
+            </li>
+            <li>
+                {restaurants[1].reviews[2].text}
+            </li>
+        </ul>
+    </div>
+    <div>
+        <h2>{restaurants[2].name}</h2>
+        <h3>Меню</h3>
+        <ul>
+            <li>
+                {restaurants[2].menu[0].name}
+            </li>
+            <li>
+                {restaurants[2].menu[1].name}
+            </li>
+            <li>
+                {restaurants[2].menu[2].name}
+            </li>
+        </ul>
+        <h3>Отзывы</h3>
+        <ul>
+            <li>
+                {restaurants[2].reviews[0].text}
+            </li>
+        </ul>
+    </div>
+    <div>
+        <h2>{restaurants[3].name}</h2>
+        <h3>Меню</h3>
+        <ul>
+            <li>
+                {restaurants[3].menu[0].name}
+            </li>
+            <li>
+                {restaurants[3].menu[1].name}
+            </li>
+        </ul>
+        <h3>Отзывы</h3>
+        <ul>
+            <li>
+                {restaurants[3].reviews[0].text}
+            </li>
+            <li>
+                {restaurants[3].reviews[1].text}
+            </li>
+        </ul>
+    </div>
+</main>);
