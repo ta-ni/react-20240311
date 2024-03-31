cda26a59a7277cd11449c56b30c9cacdd80c324e
ta-ni <t.klymets@gmail.com>
2024-04-01 00:04:06 +0100

    add changes after lecture 4. Create and use the hook useCount.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Layout } from "./components/layout/component.jsx";
 import { RestaurantTabs } from "./components/restaurant-tabs/component.jsx";
 import { Restaurant } from "./components/restaurant/component.jsx";
 import { restaurants } from "./constants/mock.js";
-
-const localStorageKeys = {
-    CURRENT_RESTAURANT_INDEX: "current-restaurant-index"
-}
+import { localStorageKeys } from "./constants/storage-keys.js";
+import { getStorageItem, setStorageItem } from "./utils/storage.js";
 
 export const App = () => {
     const [currentRestaurantIndex, setCurrentRestaurantIndex] = useState(() => {
-        return Number(localStorage.getItem(localStorageKeys.CURRENT_RESTAURANT_INDEX));
+        return Number(getStorageItem(localStorageKeys.CURRENT_RESTAURANT_INDEX));
     });
     const currentRestaurant = restaurants[currentRestaurantIndex];
-
-    useEffect(() => {
-        localStorage.setItem(localStorageKeys.CURRENT_RESTAURANT_INDEX, currentRestaurantIndex);
-    }, [currentRestaurantIndex]);
-
 
     return (
         <Layout>
             <RestaurantTabs
                 restaurants={restaurants}
-                onTabClick={setCurrentRestaurantIndex}
+                onTabClick={(index) => {
+                    setCurrentRestaurantIndex(index);
+                    setStorageItem(localStorageKeys.CURRENT_RESTAURANT_INDEX, index);
+                }}
                 currentIndex={currentRestaurantIndex}
             />
             {<Restaurant restaurant={currentRestaurant}/>}
